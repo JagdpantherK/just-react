fa0378fcc1bd798932bdbebd7c4536cb90f01d1b
JagdpantherK <jagpanthertitnium@gmail.com>
2022-12-19 17:33:10 +0600

    Updates

---
+++ precache-manifest.855e688b95982137f1fe027fa499b498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a665e4a21125d89f57a2",
+    "url": "/just-react/static/css/main.596e209e.chunk.css"
+  },
+  {
+    "revision": "a665e4a21125d89f57a2",
+    "url": "/just-react/static/js/main.5d2b3220.chunk.js"
+  },
+  {
+    "revision": "81cc757bebaaba572286",
+    "url": "/just-react/static/js/runtime~main.b2b21cc2.js"
+  },
+  {
+    "revision": "614d6e4b80538093a5e4",
+    "url": "/just-react/static/js/2.0d398c9a.chunk.js"
+  },
+  {
+    "revision": "eac2663a493c35f8869d",
+    "url": "/just-react/static/js/3.a2d6af6c.chunk.js"
+  },
+  {
+    "revision": "7819d9ee1f206ddaaf851d84c9665d87",
+    "url": "/just-react/index.html"
+  }
+];
